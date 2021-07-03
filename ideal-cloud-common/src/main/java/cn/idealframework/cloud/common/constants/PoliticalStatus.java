755586f2a324872ef303dd1ba34b711d361e83f0
宋志宗 <zzsong91@163.com>
2021-07-04 01:56:11 +0800

    add PoliticalStatus

---
+++ ideal-cloud-common/src/main/java/cn/idealframework/cloud/common/constants/PoliticalStatus.java
@@ -0,0 +1,48 @@
+package cn.idealframework.cloud.common.constants;
+
+/**
+ * @author 宋志宗 on 2021/7/3
+ */
+public enum PoliticalStatus {
+  /** 无 */
+  NONE,
+
+  /** 群众 */
+  MASSES,
+
+  /** 中共党员 */
+  CPC,
+
+  /** 中共预备党员 */
+  CPC_PROBATIONARY,
+
+  /** 共青团员 - 共产主义青年团 */
+  GQT,
+
+  /** 民革党员 - 中国国民党革命委员会 */
+  MDC,
+
+  /** 民盟盟员 - 中国民主同盟 */
+  MZTM,
+
+  /** 民建会员 - 中国民主建国会 */
+  MZJGH,
+
+  /** 民进会员 - 中国民主促进会 */
+  MZCJH,
+
+  /** 农工党党员 - 中国农工民主党 */
+  NGMZD,
+
+  /** 致公党党员 - 中国致公党 */
+  ZGD,
+
+  /** 九三学社社员 - 九三学社 */
+  JSXS,
+
+  /** 台盟盟员 - 台湾民主自治同盟 */
+  TWMZZZTM,
+
+  /** 无党派人士 */
+  NONPARTY,
+}
